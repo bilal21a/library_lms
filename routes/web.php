f384c6fde3de8cacd91b77acba55d80d37f5b55b
Bilal Anwar <b.anwar21.a@gmail.com>
2023-03-19 00:46:50 +0500

    routes fix

--- routes/web.php
+++ routes/web.php
@@ -17,33 +17,12 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-
-// // user Management
-// Route::get('/user_management', [UserController::class, 'index'])->name('user_management');
-// Route::get('/get_users', [UserController::class, 'get_data'])->name('get_users');
-// Route::get('/user_add_show', [UserController::class, 'add_form'])->name('user_add_show');
-// Route::post('/add_user', [UserController::class, 'save_data'])->name('add_user');
-// Route::get('/edit_user/{id}', [UserController::class, 'edit_form'])->name('edit_user');
-// Route::post('/update_user', [UserController::class, 'update_data'])->name('update_user');
-// Route::get('/delete_user/{id}', [UserController::class, 'delete_data'])->name('delete_user');
-
-// Users Crud
+// Users Management
 Route::resource('users', UserController::class);
 Route::get('get_users', [App\Http\Controllers\UserController::class, 'get_data'])->name('get_users');
-// Author Crud
+// Author Management
 Route::resource('author', AuthorController::class);
 Route::get('get_author', [App\Http\Controllers\AuthorController::class, 'get_data'])->name('get_author');
-
-
-
-// Route::get('/authors', [UserController::class, 'index'])->name('authors');
-// Route::get('/user_add_show', [UserController::class, 'add_form'])->name('user_add_show');
-// Route::post('/add_user', [UserController::class, 'save_data'])->name('add_user');
-// Route::get('/edit_user/{id}', [UserController::class, 'edit_form'])->name('edit_user');
-// Route::post('/update_user', [UserController::class, 'update_data'])->name('update_user');
-// Route::get('/delete_user/{id}', [UserController::class, 'delete_data'])->name('delete_user');
-
-
 
 Auth::routes();
 
